4235a85b70c9194b4f30278cce251d8a0990bacc
Charlie Hagedorn <cah49@uw.edu>
2013-07-04 15:51:55 -0700

    Yukawa With Linear simultaneous fit added. Yukawa fitting code modified/simplified. May have introduced bugs; not yet sure.

--- bootstrap/run3147simplexBootstrapYukawaSQP.m
+++ bootstrap/run3147simplexBootstrapYukawaSQP.m
@@ -7,20 +7,14 @@ if( ~exist('pressEncP'))
 	pressEncP = load([HOMEDIR 'calibration/pressure/run2937pressEncOutput.dat']);
 end
 
-
+%Need to put these somewhere.
 bootstrapOut = [];
-
 slopeOut = [];
 
-more off; 
-
-
-
-
-%pM(:,torqueCol) = randn(rows(pM),1)*1e-15;
-
+%Load analyzed torque data
 pM = load([HOMEDIR 'runAnalysis/results/run3147pM3FilterMerge.dat']);
 
+%Inject random data to preserve the blind. 
 if( max(abs(pM(:,torqueCol)) == 0) )
 	if( exist('unBlind') & unBlind == 0)
 		pM(:,torqueCol) = randn(rows(pM),1).*pM(:,torerrCol);
@@ -28,6 +22,7 @@ if( max(abs(pM(:,torqueCol)) == 0) )
 		error('blind persists, but blind should not persist');
 	end
 else
+%Interlock to prevent unblinding
 	if( exist('unBlind') & unBlind == 0)
 		if(fakeTheData == 0)
 			error('Blind VIOLATED! WTF, MATE? This error should never happen');
@@ -38,163 +33,134 @@ else
 	end
 end
 
+%These are the data which will be fit.
 dBSArchive = [pM(:,aCol) pM(:,bCol) pM(:,torCol) pM(:,torerrCol)];
 dBSArchive = dBSArchive(dBSArchive(:,4) < torErrThresh,:);
 dBSArchive = dBSArchive(abs(dBSArchive(:,3)) < torErrThresh,:);
 
+
+%Post-analysis signal injection.
 testInjection = 0 
 if( testInjection == 1)
-	alpha = 10
-	lambda = 80e-6
+	alpha = 100
+	lambda = 100e-6
 	yo = yukawaForceLaw(alpha, lambda, 1e-6, 3e-3, 1e-6);
 
-	(touch2937 - polyval(pressEncP, dBSArchive(:,1)))*1e-6
 	interp1(yo(:,1), yo(:,2), (touch2937 - polyval(pressEncP, dBSArchive(:,1)))*1e-6)
 	dBSArchive(:,3) = interp1(yo(:,1), yo(:,2), (touch2937 - polyval(pressEncP, dBSArchive(:,1)))*1e-6) - interp1(yo(:,1), yo(:,2), (touch2937 - polyval(pressEncP,dBSArchive(:,2)))*1e-6) + randn(rows(dBSArchive), 1).*dBSArchive(:,4);
 
 end
 
-dBSArchive(:,3)
-
-
+%Sanity check
 if rows(dBSArchive) < 2
 	error('Insufficient data. Wrong channel? Cut too hard?');
 end
 
-pause = 0 ; 
+pause = 0; 
 
+
+%Bootstrap loop
 for bootStrapCounter = 1:30000
 
-bootStrapCounter
+	bootStrapCounter
 
-d = bootstrapData(dBSArchive);
+	d = bootstrapData(dBSArchive);
 
-'bootstrapping complete'
+	'bootstrapping complete'
 
-%d = d( abs(d(:,3))< 5e-14 ,:);
+	%d = d( abs(d(:,3))< 5e-14 ,:);
 
-d(:,1) = touch2937 - polyval(pressEncP, d(:,1));
-d(:,2) = touch2937 - polyval(pressEncP, d(:,2));
+	d(:,1) = touch2937 - polyval(pressEncP, d(:,1));
+	d(:,2) = touch2937 - polyval(pressEncP, d(:,2));
 
-%Save the real data
-dArchive = d; 
-%plot3(dArchive(:,1), dArchive(:,2), dArchive(:,3),'+')
-
+	%Save the real data
+	dArchive = d; 
 
 
-pause
+	%Crude linear fit
+	x = d(:,1:2);					      %independent variables
 
+	y = d(:,3);                                  %Dependent variable
+	%
+	%Nonlinear least squares regression
+	pin=1e-15*ones(1,1);                                    %There are 7 parameters
 
-x = d(:,1:2);					      %independent variables
+	pMMeanTorque = mean(y);
 
-y = d(:,3);                                  %Dependent variable
-%
-%Nonlinear least squares regression
-pin=1e-15*ones(1,1);                                    %There are 7 parameters
-
-pMMeanTorque = mean(y);
-
-% Function to fit
-%F = inline("p(1)*(x(:,1)-x(:,2)) - pMMeanTorque", "x", "p");
-%F = inline("p(1)*(x(:,1)-x(:,2)) - 3", "x", "p");
-F = @(x,p) p(1)*(x(:,1)-x(:,2)) - pMMeanTorque;
-%F = @(x,p,m) p(1)*(x(:,1)-x(:,2)) - m;
-% This is the Octave fitting routine
-%[f,p,kvg,iter,corp,covp,covr,stdresid,Z,r2]=leasqr(x,y,pin,F,1e-6);
-% End of script
+	% Function to fit
+	F = @(x,p) p(1)*(x(:,1)-x(:,2)) - pMMeanTorque;
+	% This is the Octave fitting routine
+	%[f,p,kvg,iter,corp,covp,covr,stdresid,Z,r2]=leasqr(x,y,pin,F,1e-6);
+	% End of script
 
 
 
-z = [];
-q = [];
+	z = [];
+	q = [];
 
-%
-'beginning fit'
-for p = -2e-17:1e-20:2e-17
-	z = [z sum( (F(x,p) - y).^2)];
+	%
+	'beginning fit'
+	for p = -2e-17:1e-20:2e-17
+		z = [z sum( (F(x,p) - y).^2)];
 
-	q = [q p];
-end
+		q = [q p];
+	end
 
-%plot(q,z)
+	[qmin qmindex] = min(z);
 
-%pause
+	p = q(qmindex) 
+	slopeOut = [slopeOut; p];
 
-[qmin qmindex] = min(z);
+	d(:,3) = d(:,3) - F( x, p) ; 
 
-p = q(qmindex) 
-slopeOut = [slopeOut; p];
+	errorEstimate = std(d(:,3));
 
-%From square wave
-%p = -4.23e-15/545.58
-
-%from floating bootstrap
-%p = -6.9032e-18
-
-%%plot3(x(:,1), x(:,2), y,'+1')
-%hold on
-%plot3(x(:,1), x(:,2), F(x,p),'+3');
-%hold off
-
-%pause
-
-d(:,3) = d(:,3) - F( x, p) ; 
-
-errorEstimate = std(d(:,3));
-
-dSci = dArchive; 
+	dSci = dArchive; 
 
 
-%pMSci = pMSci(1:1334,:);
-dSci = dSci(dSci(:,1) >= pfTouch+10,:);
-dSci = dSci(dSci(:,2) >= pfTouch+10,:);
-%dSci = dSci(dSci(:,1) < 500,:);
-%dSci = dSci(dSci(:,2) < 500,:);
-%pMSci = pMSci(pMSci(:,psB+9) > 30,:);
-%dSci = chuckDataLine (dSci, 3);  %%%%% IS THIS LEGAL?! <-- probably, especially if only applied once. 
+	%pMSci = pMSci(1:1334,:);
+	dSci = dSci(dSci(:,1) >= pfTouch+10,:);
+	dSci = dSci(dSci(:,2) >= pfTouch+10,:);
+	%dSci = dSci(dSci(:,1) < 500,:);
+	%dSci = dSci(dSci(:,2) < 500,:);
 
-%xSci = [polyval(dutyencp, pMSci(:,psA+9)) polyval(dutyencp, pMSci(:,psB+9)) ]; 
-xSci = dSci(:,1:2);
+	xSci = dSci(:,1:2);
 
-%% UNCOMMENT ME!!!
-dSci(:,3) = dSci(:,3) - F( xSci, p) ; 
+	%% UNCOMMENT ME!!!
+	%dSci(:,3) = dSci(:,3) - F( xSci, p) ; 
 
 
-%plot3(xSci(:,1), xSci(:,2), dSci(:,3),'+1')
-%hold on
-%plot3(xSci(:,1), xSci(:,2), 0.*xSci(:,1),'+3');
-%hold off
+	preData = [xSci(:,2)*1e-6, xSci(:,1)*1e06 , dSci(:,3), dSci(:,4)];
 
-data = [xSci(:,2), xSci(:,1) , dSci(:,3), 1.0*dSci(:,4)];
-
-
-preData= [ data(:,1)*1e-6, data(:,2)*1e-6, data(:,3),data(:,4)];
-
-%lambdas, alphas
-cSFunc = @(x) chiSquareVectorYukawa(preData, x(1), x(2));
+	%lambdas, alphas
+	%cSFunc = @(x) chiSquareVectorYukawa(preData, x(1), x(2));
+	cSFunc = @(x) chiSquareVectorYukawaWSlope(preData, x(1), x(2), x(3));
 
 
-try
-ranSeed = [ 10^( rand*3.0-6) , (-1).^(round(rand)+1)*10^(rand*11-5)]
-%[x, csMax, nf] = nmsmax(cSFunc, ranSeed, [1e-10 inf inf 0 0 1]);
-%[x, csMin, info, iter, nf]   = sqp(ranSeed, cSFunc, [], [], [0, -realmax], [1, realmax])
-[x, csMin, info, iter, nf]   = sqp(ranSeed, cSFunc, [], [], [0, -realmax], [1, realmax],2000,1e-20)
+	%Fit begins
+	ranSeed = [ 10^( rand*3.0-6) , (-1).^(round(rand)+1)*10^(rand*11-5), (rand-0.5)*10^-15]
+	%ranSeed = [ 10^( rand*3.0-6) , (-1).^(round(rand)+1)*10^(rand*11-5)]
+	try
+		%[x, csMax, nf] = nmsmax(cSFunc, ranSeed, [1e-10 inf inf 0 0 1]);
+		%[x, csMin, info, iter, nf]   = sqp(ranSeed, cSFunc, [], [], [0, -realmax], [1, realmax])
+		%[x, csMin, info, iter, nf]   = sqp(ranSeed, cSFunc, [], [], [0, -realmax], [1, realmax],2000,1e-20)
 
-%When analyzing, consider a cut on csMin
-bsO = [ x(1) x(2) csMin nf iter info ranSeed];
+		[x, csMin, info, iter, nf]   = sqp(ranSeed, cSFunc, [], [], [0, -realmax,-realmax], [.01, realmax, realmax],2000,1e-20)
 
-%if fit converged, save it.
-if(info == 101) 
-	bootstrapOut(bootStrapCounter,:) = bsO; 
-end
+		%When analyzing, make a cut on csMin
+		%bsO = [ x(1) x(2) csMin nf iter info ranSeed];
+		bsO = [ x(1) x(2) csMin nf iter info ranSeed x(3)];
 
-save 'run3147simplexBootstrapYukawa.floatLin10ChopLongSQPSlope.dat' slopeOut
-save 'run3147simplexBootstrapYukawa.floatLin10ChopLongSQP.dat' bootstrapOut
-catch
-	'FIT ERROR! BED HAS BEEN POOPED!'
-	errorMessage
-end
+		%if fit converged, save it.
+		if(info == 101) 
+			bootstrapOut(bootStrapCounter,:) = bsO; 
+		end
+
+		save 'run3147simplexBootstrapYukawa.floatLin10ChopLongSQPSlope.dat' slopeOut
+		save 'run3147simplexBootstrapYukawa.SimulFloat10ChopLongSQP.dat' bootstrapOut
+	catch
+		'FIT ERROR! BED HAS BEEN POOPED!'
+		errorMessage
+	end
 
 end %bsCnt
-
-
---
+++ mlib/chiSquareVectorYukawaWSlope.m
@@ -0,0 +1,14 @@
+function [cS yTD] = chiSquareVectorYukawaWSlope(inOutTorqueTorqueErr, lambdas, alphas, slope)
+
+	d=inOutTorqueTorqueErr;
+
+	%Note that not transposing d is important
+	measTorques = repmat(d(:,3), [1 length(lambdas) length(alphas)]);
+	torqueErr   = repmat(d(:,4), [1 length(lambdas) length(alphas)]);
+
+	yTD =  (yukawaVectorizedTorque(d(:,1)', lambdas , alphas) - yukawaVectorizedTorque(d(:,2)', lambdas, alphas) + slope.*(d(:,1) - d(:,2)) - \
+		measTorques).^2 .* (1./torqueErr).^2;
+
+	cS=squeeze(sum(yTD,1));
+
+end
