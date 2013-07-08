7fffdefb9c2bc9d700f307c8039b42ddc9ff8e25
Charlie Hagedorn <cah49@uw.edu>
2013-07-08 15:31:05 -0700

    Working away on fitting. Couple mlib changes, big simplification of SQP fit. Now implementing NMS as well.

--- bootstrap/run3147simplexBootstrapYukawaSQP.m
+++ bootstrap/run3147simplexBootstrapYukawaSQP.m
@@ -33,23 +33,49 @@ else
 	end
 end
 
+
 %These are the data which will be fit.
 dBSArchive = [pM(:,aCol) pM(:,bCol) pM(:,torCol) pM(:,torerrCol)];
-dBSArchive = dBSArchive(dBSArchive(:,4) < torErrThresh,:);
+
+%Calibrate distance
+dBSArchive(:,1) = (touch2937 - polyval(pressEncP, dBSArchive(:,1)) ) * 1e-6;
+dBSArchive(:,2) = (touch2937 - polyval(pressEncP, dBSArchive(:,2)) ) * 1e-6;
+%dBSArchive(:,1:2) = (touch2937 - polyval(pressEncP, dBSArchive(:,1:2)) ) * 1e-6;
+
+%Torque threshold cut
+dBSArchive = dBSArchive(dBSArchive(:,4)      < torErrThresh,:);
 dBSArchive = dBSArchive(abs(dBSArchive(:,3)) < torErrThresh,:);
 
 
+%Distance cut
+shortCut = (pfTouch+10)*1e-6
+dBSArchive = dBSArchive(dBSArchive(:,1) >= shortCut,:);
+dBSArchive = dBSArchive(dBSArchive(:,2) >= shortCut,:);
+%dSci = dSci(dSci(:,1) < 500,:);
+%dSci = dSci(dSci(:,2) < 500,:);
+
 %Post-analysis signal injection.
-testInjection = 0 
 if( testInjection == 1)
-	alpha = 100
-	lambda = 100e-6
+	alpha    = 100
+	lambda 	 = 100e-6
+	injSlope = 2e-12 
 	yo = yukawaForceLaw(alpha, lambda, 1e-6, 3e-3, 1e-6);
 
-	interp1(yo(:,1), yo(:,2), (touch2937 - polyval(pressEncP, dBSArchive(:,1)))*1e-6)
-	dBSArchive(:,3) = interp1(yo(:,1), yo(:,2), (touch2937 - polyval(pressEncP, dBSArchive(:,1)))*1e-6) - interp1(yo(:,1), yo(:,2), (touch2937 - polyval(pressEncP,dBSArchive(:,2)))*1e-6) + randn(rows(dBSArchive), 1).*dBSArchive(:,4);
+	injPos = dBSArchive(:,1:2)
+
+	dBSArchive(:,3) = interp1(yo(:,1), yo(:,2), injPos(:,1)) - interp1(yo(:,1), yo(:,2), injPos(:,2)) + randn(rows(dBSArchive), 1).*dBSArchive(:,4) + injSlope*(injPos(:,1) - injPos(:,2)),;
 
 end
+
+clear pause
+ 
+plot(dBSArchive(:,1), dBSArchive(:,2),'+');
+pause
+plot(pM(:,aCol), pM(:,bCol),'+');
+pause
+
+plot3(dBSArchive(:,1), dBSArchive(:,2), dBSArchive(:,3),'+');
+pause
 
 %Sanity check
 if rows(dBSArchive) < 2
@@ -57,7 +83,7 @@ if rows(dBSArchive) < 2
 end
 
 pause = 0; 
-
+nmsFit = 1
 
 %Bootstrap loop
 for bootStrapCounter = 1:30000
@@ -68,96 +94,48 @@ for bootStrapCounter = 1:30000
 
 	'bootstrapping complete'
 
-	%d = d( abs(d(:,3))< 5e-14 ,:);
-
-	d(:,1) = touch2937 - polyval(pressEncP, d(:,1));
-	d(:,2) = touch2937 - polyval(pressEncP, d(:,2));
-
 	%Save the real data
-	dArchive = d; 
-
-
-	%Crude linear fit
-	x = d(:,1:2);					      %independent variables
-
-	y = d(:,3);                                  %Dependent variable
-	%
-	%Nonlinear least squares regression
-	pin=1e-15*ones(1,1);                                    %There are 7 parameters
-
-	pMMeanTorque = mean(y);
-
-	% Function to fit
-	F = @(x,p) p(1)*(x(:,1)-x(:,2)) - pMMeanTorque;
-	% This is the Octave fitting routine
-	%[f,p,kvg,iter,corp,covp,covr,stdresid,Z,r2]=leasqr(x,y,pin,F,1e-6);
-	% End of script
-
-
-
-	z = [];
-	q = [];
-
-	%
-	'beginning fit'
-	for p = -2e-17:1e-20:2e-17
-		z = [z sum( (F(x,p) - y).^2)];
-
-		q = [q p];
-	end
-
-	[qmin qmindex] = min(z);
-
-	p = q(qmindex) 
-	slopeOut = [slopeOut; p];
-
-	d(:,3) = d(:,3) - F( x, p) ; 
-
-	errorEstimate = std(d(:,3));
-
-	dSci = dArchive; 
-
-
-	%pMSci = pMSci(1:1334,:);
-	dSci = dSci(dSci(:,1) >= pfTouch+10,:);
-	dSci = dSci(dSci(:,2) >= pfTouch+10,:);
-	%dSci = dSci(dSci(:,1) < 500,:);
-	%dSci = dSci(dSci(:,2) < 500,:);
-
-	xSci = dSci(:,1:2);
-
-	%% UNCOMMENT ME!!!
-	%dSci(:,3) = dSci(:,3) - F( xSci, p) ; 
-
-
-	preData = [xSci(:,2)*1e-6, xSci(:,1)*1e06 , dSci(:,3), dSci(:,4)];
 
 	%lambdas, alphas
-	%cSFunc = @(x) chiSquareVectorYukawa(preData, x(1), x(2));
-	cSFunc = @(x) chiSquareVectorYukawaWSlope(preData, x(1), x(2), x(3));
-
+	if (nmsFit == 1)
+		cSFunc = @(x) -chiSquareVectorYukawaWSlope(d, x(1), x(2), x(3));
+	else
+		cSFunc = @(x) chiSquareVectorYukawaWSlope(d, x(1), x(2), x(3));
+	end
 
 	%Fit begins
-	ranSeed = [ 10^( rand*3.0-6) , (-1).^(round(rand)+1)*10^(rand*11-5), (rand-0.5)*10^-15]
-	%ranSeed = [ 10^( rand*3.0-6) , (-1).^(round(rand)+1)*10^(rand*11-5)]
+	ranSeed = [ 10^( rand*3.0-6) , (-1).^(round(rand)+1)*10^(rand*11-5), (rand-0.5)*10^-11]
 	try
-		%[x, csMax, nf] = nmsmax(cSFunc, ranSeed, [1e-10 inf inf 0 0 1]);
-		%[x, csMin, info, iter, nf]   = sqp(ranSeed, cSFunc, [], [], [0, -realmax], [1, realmax])
-		%[x, csMin, info, iter, nf]   = sqp(ranSeed, cSFunc, [], [], [0, -realmax], [1, realmax],2000,1e-20)
-
-		[x, csMin, info, iter, nf]   = sqp(ranSeed, cSFunc, [], [], [0, -realmax,-realmax], [.01, realmax, realmax],2000,1e-20)
-
 		%When analyzing, make a cut on csMin
-		%bsO = [ x(1) x(2) csMin nf iter info ranSeed];
-		bsO = [ x(1) x(2) csMin nf iter info ranSeed x(3)];
+		if(nmsFit == 1)
+			[x, csMax, nf] = nmsmax(cSFunc, ranSeed, [1e-15 inf inf 0 0 1]);
+			bsO = [ x(1) x(2) csMax nf ranSeed x(3)];
+		else
+			[x, csMin, fitInfo, iter, nf]   = sqp(ranSeed, cSFunc, [], [], [0, -realmax,-realmax], [.01, realmax, realmax],2000,1e-22)
+			bsO = [ x(1) x(2) csMin nf iter fitInfo ranSeed x(3)];
+		end
+
 
 		%if fit converged, save it.
-		if(info == 101) 
+		if(exist('fitInfo') && fitInfo == 101) 
 			bootstrapOut(bootStrapCounter,:) = bsO; 
 		end
 
-		save 'run3147simplexBootstrapYukawa.floatLin10ChopLongSQPSlope.dat' slopeOut
-		save 'run3147simplexBootstrapYukawa.SimulFloat10ChopLongSQP.dat' bootstrapOut
+		if(nmsFit == 1 )
+			bootstrapOut(bootStrapCounter,:) = bsO; 
+		end
+
+		al   = num2str(alpha);
+		lam  = num2str(lambda);
+		injS = num2str(injSlope);
+		
+		if(nmsFit == 1 )
+			fitType = 'NMS'
+		else
+			fitType = 'SQP'
+		end
+		outfilename = ['run3147simplexBootstrapYukawa.SimulFloata' al 'l' lam 'slop' injS fitType '.dat'];
+		save( outfilename, "bootstrapOut");
 	catch
 		'FIT ERROR! BED HAS BEEN POOPED!'
 		errorMessage
--- mlib/errorMessage.m
+++ mlib/errorMessage.m
@@ -1,2 +1,2 @@
-printf('Error. Message: %s\nFile: %s\nLine: %s\nScope: %s\nContext: %s\n' , lasterror.message , lasterror.stack.file, lasterror.stack.line , lasterror.stack.scope , lasterror.stack.context);
+printf('Error. Message: %s\nFile: %s\nLine: %s\nScope: %s\nContext: %s\n' , lasterror.message , lasterror.stack.file, num2str(lasterror.stack.line) , lasterror.stack.scope , lasterror.stack.context);
 
