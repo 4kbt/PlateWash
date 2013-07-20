2dd47d8926513626c52eda926dad3bc89679dd91
Charlie Hagedorn <cah49@uw.edu>
2013-07-19 18:06:18 -0700

    Force law modified to make arbFit plots work right.

--- bootstrap/postLockinSignalInjection.m
+++ bootstrap/postLockinSignalInjection.m
@@ -15,8 +15,10 @@ if( testInjection == 1)
 	%Make force law
 	yo = yukawaForceLaw(alpha, lambda, 1e-6, 3e-3, 1e-6);
 
+	yo(:,2) = yo(:,1)*injSlope + yo(:,2);
+
 	%Fake it!
 	dBSArchive(:,3) = interp1(yo(:,1), yo(:,2), dBSArchive(:,1)) - interp1(yo(:,1), yo(:,2), dBSArchive(:,2))\
-		          + randn(rows(dBSArchive), 1).*dBSArchive(:,4) + injSlope*(dBSArchive(:,1) - dBSArchive(:,2)),;
+		          + randn(rows(dBSArchive), 1).*dBSArchive(:,4);
 end
 
