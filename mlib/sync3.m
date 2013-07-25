3138affe54895257d8ea243f77e1deeb5926a9a3
Charlie Hagedorn <cah49@uw.edu>
2013-07-25 15:44:30 -0700

    Moved sampletime mismatch within sync3 to resolve a bug for 3164sync3. Commited alone in case it breaks something.

--- mlib/sync3.m
+++ mlib/sync3.m
@@ -9,15 +9,6 @@ plot(starts, '1-+;starts;', stops, '3-+;stops;');
 xlabel('platewash, plateslave, ifo');
 ylabel('time (seconds)');
 
-pwSampleTime = median(diff(pwData(:,pwTimeCol)));
-psSampleTime = median(diff(psData(:,psTimeCol)));
-ifoSampleTime = median(diff(ifoData(:,ifoTimeCol)));
-
-if(abs(pwSampleTime-psSampleTime)>1e-1 | abs(ifoSampleTime - psSampleTime)>1e-1)
-	error("Sample Times do not match");
-end
-
-
 psOverlapStartIndex  = find( (psData (:,psTimeCol ) >= overlapStartSec) ) (1);
 pwOverlapStartIndex  = find( (pwData (:,pwTimeCol ) >= overlapStartSec) ) (1);
 ifoOverlapStartIndex = find( (ifoData(:,ifoTimeCol) >= overlapStartSec) ) (1);
@@ -30,5 +21,14 @@ ifoOverlapEndIndex = find( (ifoData(:,ifoTimeCol) >= overlapEndSec) ) (1);
 pwData  =  pwData(  pwOverlapStartIndex  : pwOverlapEndIndex  , : );
 psData  =  psData(  psOverlapStartIndex  : psOverlapEndIndex  , : );
 ifoData =  ifoData( ifoOverlapStartIndex : ifoOverlapEndIndex , : );
+
+pwSampleTime = median(diff(pwData(:,pwTimeCol)));
+psSampleTime = median(diff(psData(:,psTimeCol)));
+ifoSampleTime = median(diff(ifoData(:,ifoTimeCol)));
+
+if(abs(pwSampleTime-psSampleTime)>1e-1 | abs(ifoSampleTime - psSampleTime)>1e-1)
+	error("Sample Times do not match");
+end
+
 
 
