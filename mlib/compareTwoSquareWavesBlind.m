962afd4b0af12bbf3cf32b1ff93e93c90083f807
Charlie Hagedorn <cah49@uw.edu>
2013-08-26 15:23:11 -0700

    Magnetic systematics v0 working. No big signal! Woo.

--- mlib/compareTwoSquareWavesBlind.m
+++ mlib/compareTwoSquareWavesBlind.m
@@ -1,6 +1,6 @@
 %Evaluates the difference of a-b. Attempts to preserve the blind.
-
-function [abDiff aBins aH bBins bH]  = compareTwoSquareWavesBlind( a, b, sciCol, scierrCol, sciErrThresh, sciErrMin, lockCol, numSensors);
+%*pMPosition denotes the location of the variable within the pM data structure. A should be 1, and B should be 2 in most reasonable calls.
+function [abDiff aBins aH bBins bH aLockPos bLockPos]  = compareTwoSquareWavesBlind( a, b, sciCol, scierrCol, sciErrThresh, sciErrMin, lockCol, numSensors, ApMPosition, BpMPosition)
 
 %Clean up signs (fixes any sign ambiguity)
 a(:,sciCol) = a(:, sciCol) .* sign(a(:, lockCol));
@@ -38,10 +38,18 @@ bDev = bTor(2) * sqrt(rows(b));
 aBins = (min(a(:,sciCol)):aDev:max(a(:,sciCol)));
 bBins = (min(b(:,sciCol)):bDev:max(b(:,sciCol)));
 
+%histograms
 [aH aX] = hist(a(:,sciCol),aBins);
 [bH bX] = hist(b(:,sciCol),bBins);
 
+%rows to columns
 aH = aH'; aBins = aBins';
 bH = bH'; bBins = bBins';
+
+%Return the positions of all the differences for future plotability.
+aLockPos = [ a(:, lockCol + ApMPosition * numSensors)  \
+	     a(:, lockCol + BpMPosition * numSensors) ];
+bLockPos = [ b(:, lockCol + ApMPosition * numSensors)  \
+	     b(:, lockCol + BpMPosition * numSensors) ];
 
 end
--- systematics/magnetic/coilOnCanShortStroke.m
+++ systematics/magnetic/coilOnCanShortStroke.m
@@ -1,40 +1,41 @@
 run3147FixedParameters
 
 %Red on Right
-load('../runAnalysis/alwaysUnblindedResults/run3203pM3FilterOnly.dat');
+load(strcat( HOMEDIR , 'runAnalysis/alwaysUnblindedResults/run3203pM3FilterOnly.dat'));
 bR = pM;
 
 %Red on Left
-load('../runAnalysis/alwaysUnblindedResults/run3204pM3FilterOnly.dat');
+load(strcat( HOMEDIR , 'runAnalysis/alwaysUnblindedResults/run3204pM3FilterOnly.dat'));
 bL = pM;
 
-load('../runAnalysis/alwaysUnblindedResults/run3205pM3FilterOnly.dat');
+load(strcat( HOMEDIR , 'runAnalysis/alwaysUnblindedResults/run3205pM3FilterOnly.dat'));
 bL = [bL; pM];
 
 %Zero applied field
 b0 = [];
-load('../runAnalysis/alwaysUnblindedResults/run3161pM3FilterOnly.dat');
+load(strcat( HOMEDIR, 'runAnalysis/alwaysUnblindedResults/run3161pM3FilterOnly.dat'));
 b0 = [b0; pM];
 
-load('../runAnalysis/alwaysUnblindedResults/run3163pM3FilterOnly.dat');
+load(strcat( HOMEDIR, 'runAnalysis/alwaysUnblindedResults/run3163pM3FilterOnly.dat'));
 b0 = [b0; pM];
 
-load('../runAnalysis/alwaysUnblindedResults/run3164pM3FilterOnly.dat');
+load(strcat( HOMEDIR, 'runAnalysis/alwaysUnblindedResults/run3164pM3FilterOnly.dat'));
 b0 = [b0; pM];
 
-load('../runAnalysis/alwaysUnblindedResults/run3166pM3FilterOnly.dat');
+load(strcat( HOMEDIR, 'runAnalysis/alwaysUnblindedResults/run3166pM3FilterOnly.dat'));
 b0 = [b0; pM];
 
 clear pM;
 
-[lrDiff blBins blH brBins brH] = compareTwoSquareWavesBlind( bL,bR, torCol, torerrCol, torErrThresh, torErrMin, psSquareCol, numSensors);
-[r0Diff brBins brH b0Bins b0H] = compareTwoSquareWavesBlind( bL,b0, torCol, torerrCol, torErrThresh, torErrMin, psSquareCol, numSensors);
-[l0Diff blBins blH b0Bins b0H] = compareTwoSquareWavesBlind( bR,b0, torCol, torerrCol, torErrThresh, torErrMin, psSquareCol, numSensors);
+[lrDiff blBins blH brBins brH bLPositions bRPositions] = compareTwoSquareWavesBlind( bL,bR, torCol, torerrCol, torErrThresh, torErrMin, psSquareCol, numSensors, 1, 2);
+[r0Diff brBins brH b0Bins b0H bRPositions b0Positions] = compareTwoSquareWavesBlind( bR,b0, torCol, torerrCol, torErrThresh, torErrMin, psSquareCol, numSensors, 1, 2);
+[l0Diff blBins blH b0Bins b0H bLPositions b0Positions] = compareTwoSquareWavesBlind( bL,b0, torCol, torerrCol, torErrThresh, torErrMin, psSquareCol, numSensors, 1, 2);
 
 
 %Output...
 
-filePath = [HOMEDIR 'extracted/'];
+%filePath = [HOMEDIR 'extracted/'];
+filePath = ['extracted/'];
 
 printSigError( lrDiff(1), lrDiff(2) , [filePath 'shortStrokeLRDiff.tex']);
 printSigError( r0Diff(1), r0Diff(2) , [filePath 'shortStrokeR0Diff.tex']);
@@ -48,3 +49,6 @@ save 'plots/shortStrokeolHist.dat' olHist
 save 'plots/shortStrokeorHist.dat' orHist
 save 'plots/shortStrokeo0Hist.dat' o0Hist
 
+save 'plots/shortStrokeo0Positions' b0Positions
+save 'plots/shortStrokeoLPositions' bLPositions
+save 'plots/shortStrokeoRPositions' bRPositions
