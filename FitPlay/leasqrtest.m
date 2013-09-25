a8bfc8fa3d34bb24f269c1353ac075d98a1ccc5c
Charlie Hagedorn <cah49@uw.edu>
2013-09-25 15:39:42 -0700

    Works better. Now with bootstrap, makefile.

--- FitPlay/leasqrtest.m
+++ FitPlay/leasqrtest.m
@@ -1,12 +1,12 @@
 %Multiple independent variables with leasqr. 
 
 nObs = 100;
-noiseLevel = 0.1;
+noiseLevel = 1;
 
-realParams = [1 2 3 4]' ;
+realParams = [1; 2; 3; 4] ;
 
 %Generate some independent variables
-xvals = rand( nObs, columns(realParams));
+xvals = rand( nObs, rows(realParams));
 
 %Generate some dependent observations
 yvals = daFunc(xvals, realParams);
@@ -14,9 +14,20 @@ yvals = daFunc(xvals, realParams);
 %Noise
 yvals = yvals + noiseLevel * randn(nObs, columns(yvals));
 
-ranSeed = randn(size(realParams));
+dArchive = [xvals yvals];
+bsO = [];
 
-[f x cvg iter corp covp covr stdresid Z r2] = leasqr( xvals, yvals, ranSeed, "daFunc", 1e-10, 200, ones(size(yvals)),'dfdp');
+for bsCounter = 1:1000
 
-x
+	d = bootstrapData(dArchive);
 
+
+ranSeed = 10*randn(size(realParams));
+
+[f x cvg iter corp covp covr stdresid Z r2] = leasqr( d(:,1:columns(xvals)), d(:,end), ranSeed, "daFunc", 1e-10, 200, ones(size(d(:,end))),'dfdp');
+
+	bsO = [bsO; x' r2]; 
+
+end
+
+save 'test.dat' bsO
