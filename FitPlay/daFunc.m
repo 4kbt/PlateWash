2967d66b720d28cfe0c4f23156efc0067dbfa890
Charlie Hagedorn <cah49@uw.edu>
2013-09-25 15:01:27 -0700

    Multiple independent variables working with leasqr

---
+++ FitPlay/daFunc.m
@@ -0,0 +1,6 @@
+%x is a multicolumn vector. P is a bunch of fit parameters
+function o = daFunc(x,p) 
+		o = x*p';
+end
+
+
--- FitPlay/leasqrtest.m
+++ FitPlay/leasqrtest.m
@@ -1,5 +1,22 @@
-function o = daFunc(x,p) 
-		o = x'*p
-end
+%Multiple independent variables with leasqr. 
 
+nObs = 100;
+noiseLevel = 0.1;
+
+realParams = [1 2 3 4]' ;
+
+%Generate some independent variables
+xvals = rand( nObs, columns(realParams));
+
+%Generate some dependent observations
+yvals = daFunc(xvals, realParams);
+
+%Noise
+yvals = yvals + noiseLevel * randn(nObs, columns(yvals));
+
+ranSeed = randn(size(realParams));
+
+[f x cvg iter corp covp covr stdresid Z r2] = leasqr( xvals, yvals, ranSeed, "daFunc", 1e-10, 200, ones(size(yvals)),'dfdp');
+
+x
 
