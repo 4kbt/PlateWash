4028cd7c288042aa1bce5c77283e515b6f187228
Charlie Hagedorn <cah49@uw.edu>
2013-10-08 19:11:57 -0700

    supporting plots added

---
+++ supportingPlots/run3147filterFrequencyResponse.m
@@ -0,0 +1,17 @@
+clear
+
+try
+run3147preSM3A
+catch
+end
+
+%large number yields prettier FFT.
+[rTorque  wTorque ]  = freqz(fTorque , 1, Nfilt);
+[rSensors wSensors]  = freqz(fSensors, 1, Nfilt);
+
+oTor =  [wTorque /pi*NyquistFrequency, abs(rTorque )];
+oSens = [wSensors/pi*NyquistFrequency, abs(rSensors)];
+
+save 'run3147filterResponseTorque.dat'  oTor
+save 'run3147filterResponseSensors.dat' oSens
+
