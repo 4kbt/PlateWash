059afb90219a38710c65732ddf4e7c60b4641c8d
Charlie Hagedorn <cah49@uw.edu>
2013-10-16 13:14:23 -0700

    Automated bootstrap simulation plot now works with LyX.

--- bootstrap/generatePlottingTeX.sh
+++ bootstrap/generatePlottingTeX.sh
@@ -5,9 +5,13 @@ myPathName=$1/bootstrap/
 fn=output/makeSymFigure.tex
 
 echo '\begin{figure}' > $fn
+echo '\begin{centering}' >> $fn
 for names in $(ls output/bootstrapYukawa*.SimulFloata*.eps)
 do
-	echo "\\includegraphics[height=5.4cm,angle=270]{$myPathName$names}" >> $fn
+	echo "\\includegraphics[height=0.4\\columnwidth, angle=270]{$myPathName$names}" >> $fn
 done
+echo '\par\end{centering}' >> $fn
+echo '\label{BootstrappedSignalInjection}' >> $fn
+echo '\caption{Bootstrapped fitting with signal injections}' >> $fn
 
 echo  '\end{figure}' >> $fn
