59663a2b740cd78a19a359bb30240b6d0980742a
Charlie Hagedorn <cah49@uw.edu>
2013-11-05 11:12:37 -0800

    Added resistor runs to properly-specified runs

---
+++ runConfig/run3178sync3.m
@@ -0,0 +1,16 @@
+pwrunNumber  = '3178'
+psrunNumber  = '3176';
+iforunNumber = '3138';
+
+preSync3
+
+%psStartSec  = pwStartSec+271;
+%psEndSec    = psEndSec-100;
+%psEndSec    = psEndSec - 86000*.8
+%psStartSec  = psStartSec + 2431 
+%pwEndSec    = pwStartSec + 219000; 
+%pwStartSec  = pwStartSec + 6300;
+%ifoEndSec   = ifoStartSec + 16000; 
+%ifoStartSec = ifoStartSec + 4100;
+
+sync3
