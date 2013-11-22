a6e3524a3373e24d6794d633f36686e8186568d9
Charlie Hagedorn <cah49@uw.edu>
2013-11-22 14:06:38 -0800

    apparently unnecessary stuff commented in order to make electrical work

--- systematics/electrical/electricalLongStroke.m
+++ systematics/electrical/electricalLongStroke.m
@@ -24,12 +24,13 @@ for resistorCtr = 1:rows(resistorRuns)
 
 	[aH aX] = hist(pM(:,torqueCol), aBins);
 
-
 	filePath = ['extracted/'];
 
 	printSigError( l0Diff(1), l0Diff(2) , [filePath 'longStrokeL0Diff' runString '.tex']);
 
-	save (['plots/longStroke' runString '.dat'],"outData");
+	%These don't work/don't appear to be necessary (data comes out in *.tex, see Makefile)
+	%outData =[resistorRuns(resistorCtr,:) l0Diff];
+	%save (['plots/longStroke' runString '.dat'],"outData");
 
 	olHist = [aBins' aH'];
 
