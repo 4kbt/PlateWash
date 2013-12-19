4493e84557fd4e07028cbcb316a9a5c8038061f1
Charlie Hagedorn <charlie@mcmillan>
2013-12-18 23:12:50 -0800

    More pressure edits

--- systematics/pressure/finalPressure.m
+++ systematics/pressure/finalPressure.m
@@ -1,7 +1,7 @@
+fundamentalConstants
+
 d = load('finalPressure.dat');
 
 printSigNumber(d, 'extracted/finalPressureTorr.tex', 2);
-
-OnePascalInTorr = 133.3224;
 
 printSigNumber(d*OnePascalInTorr, 'extracted/finalPressurePa.tex', 2);
