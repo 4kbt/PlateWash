42c420da37486c01c2b22b89f3f0bad539c9f597
Charlie Hagedorn <cah49@uw.edu>
2014-01-07 15:38:54 -0800

    Added XXXRC, XXXMA, XXXMI counters

--- bin/countXXXs.sh
+++ bin/countXXXs.sh
@@ -2,4 +2,4 @@
 #Counts the number of XXXs (case insensitive) in the file given in the first argument. 
 #Output is preceded by timing information in both seconds since 1970 and human-readable form.
 
-echo  `date +"%s %Y %m %d %H %M %S"` `grep -io xxx $1 | wc -l` `wc $1` ` grep "begin_inset Graphics" $1 | wc -l`
+echo  `date +"%s %Y %m %d %H %M %S"` `grep -io xxx $1 | wc -l` `wc $1` ` grep "begin_inset Graphics" $1 | wc -l` `grep -io xxxrc $1 | wc -l` `grep -io xxxma $1 | wc -l` `grep -io xxxmi $1 | wc -l`
