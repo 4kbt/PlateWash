bd18ef056dbff405c4a6ec791fb8b7bd0a09e1a8
charlie <charlie@dicke.nanoradian.com>
2014-01-15 14:37:41 -0800

    Improved transient suppression for pressEnc (who thought it need improving? Not me!)

--- calibration/pressure/run2937pressEnc.m
+++ calibration/pressure/run2937pressEnc.m
@@ -1,12 +1,15 @@
 run2937syncPS
 
 
-o = trimSpikes(psData,15,0.005,10);
+o = trimSpikes(psData,15,0.005,5,5);
 
 
 pressEncP = polyfit(o(:,15), o(:,13),1);
 
+press = [o(:,15) o(:,13) polyval(pressEncP,o(:,15))];
 
 plot(o(:,15), o(:,13),'.', o(:,15), polyval(pressEncP,o(:,15)),'.');
 
 save 'run2937pressEncOutput.dat' pressEncP
+
+save 'run2937pressEncData.dat' press
