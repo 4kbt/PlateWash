269c55bfef107e497e6dbf3a6e948ebd65214808
charlie <charlie@dicke.nanoradian.com>
2014-01-26 17:54:27 -0800

    More newtonian targets

---
+++ NewtonianSimulation/parts/jan14Flexure.m
@@ -0,0 +1,30 @@
+function Flexure = jan14Flexure()
+run3147PendulumParameters
+
+vH = flexureHeight;
+vW = flexureWidth;
+vT = wallThickness;
+vM = rhoAl * vH*vW*vT;
+
+hH = wallThickness;
+hL = flexureLength - 2 * wallThickness;
+hW = flexureWidth; 
+hM = rhoAl * vH*vW*vT; 
+
+%Make component bars
+vertBar  = genPointMassRect( vM, vT, vW, vH, 5, 5, 5);
+horizBar = genPointMassRect( hM, hL, hW, hH, 5, 5, 5);
+
+%double and place them correctly
+vB2 = [translatePMArray(vertBar, [ (flexureLength - wallThickness)/2.0, 0, 0]);\
+       translatePMArray(vertBar, [-(flexureLength - wallThickness)/2.0, 0, 0])];
+
+hB2 = [translatePMArray(horizBar, [0, 0, (flexureHeight - wallThickness)/2.0]);\
+       translatePMArray(horizBar, [0, 0,-(flexureHeight - wallThickness)/2.0])];
+
+Flexure = [vB2; hB2];
+
+Flexure = translatePMArray(Flexure, [ flexureSetBack + flexureLength/2.0,0,0]);
+
+
+end
--- globalConfig/run3147PendulumParameters.m
+++ globalConfig/run3147PendulumParameters.m
@@ -82,6 +82,39 @@ spindleMass = 3.1818e-3;
 spindleTipDistance = AttractorFullThickness;
 spindleRadialPosition = gapRadialPosition;
 
+%%% Macor Spindles %%%
+mspindleLength = 1*0.0254;		printSigNumber(mspindleLength , [HOMEDIR '/extracted/mspindleLength.tex'], 3);
+mspindleDiameter = 0.25*0.0254;		printSigNumber(mspindleDiameter,[HOMEDIR '/extracted/mspindleDiameter.tex'], 3);
+mspindleMass = 4e-3;			printSigNumber(mspindleMass    ,[HOMEDIR '/extracted/mspindleMass.tex'  ] , 3);
+
+mspindleTipDistance = spindleLength + spindleTipDistance;
+mspindleRadialPosition = spindleRadialPosition;
+
+%%% Spindle Plate %%%
+
+spindlePlateMass =      40e-3;		printSigNumber(spindlePlateMass, [HOMEDIR '/extracted/spindlePlateMass.tex'] , 3);
+spindlePlateID =        0.375*0.0254;	printSigNumber(spindlePlateID  , [HOMEDIR '/extracted/spindlePlateID.tex'  ] , 3);
+spindlePlateOD =        3.25*0.0254;	printSigNumber(spindlePlateOD  , [HOMEDIR '/extracted/spindlePlateOD.tex'  ] , 3);
+spindlePlateThickness = 0.25 * 0.0254;	printSigNumber(spindlePlateThickness, [HOMEDIR '/extracted/spindlePlateThickness.tex' ], 3);
+
+spindlePlateSetBack = mspindleTipDistance + mspindleLength; \
+					printSigNumber(spindlePlateSetBack  , [HOMEDIR '/extracted/spindlePlateSetBack.tex'   ], 3);
+
+
+%%% Flexure frame %%%
+
+rhoAl = 2700;
+
+flexureHeight = 3*0.0254;		printSigNumber(flexureHeight , [HOMEDIR '/extracted/flexureHeight.tex' ], 3);
+flexureWidth  = 1*0.0254;		printSigNumber(flexureWidth  , [HOMEDIR '/extracted/flexureWidth.tex'  ], 3);
+flexureLength = 5*0.0254;		printSigNumber(flexureLength , [HOMEDIR '/extracted/flexureLength.tex' ], 3);
+wallThickness = 0.25*0.0254;		printSigNumber(wallThickness , [HOMEDIR '/extracted/wallThickness.tex' ], 3);
+
+flexureSetBack = spindlePlateSetBack + spindlePlateThickness;
+					printSigNumber(flexureSetBack, [HOMEDIR '/extracted/flexureSetBack.tex'], 3);
+
+
+
 %%%%%% Q-tester bricks %%%%%%%
 
 %brick mass
