baf71c3809a85c0596253ae0adfb64aee918ca28
Charlie Hagedorn <cah49@uw.edu>
2014-02-04 16:44:18 -0800

    pendulum thickness handled directly from extracted value, offset torque plot generated, integrated

--- NewtonianSimulation/parts/jan13OffsetAttractor.m
+++ NewtonianSimulation/parts/jan13OffsetAttractor.m
@@ -1,7 +1,11 @@
 function Attractor = jan13OffsetAttractor
 	run3147PendulumParameters
 
-	A = jan13Attractor;
+	A = [jan13ScrewGaps;\
+		jan13Attractor;\
+		jan13Spindles;\
+		jan14MacorSpindles.dat;\
+		jan14Flexure.dat];
 
 	Attractor = translatePMArray(A, [0 attrHorizOffset attrVertOffset]);
 end
