8802f32d0417644c75700449f6ee263be3392d3c
charlie <charlie@dicke.nanoradian.com>
2014-02-18 13:42:33 -0800

    Magnetic field XXXRCs cleaned up,

--- systematics/magnetic/magneticField.m
+++ systematics/magnetic/magneticField.m
@@ -4,3 +4,8 @@ fundamentalConstants
 CoilFieldAtCenter = mu_0*CoilTurns*CoilCurrent/CoilDiameter;
 printSigNumber(CoilFieldAtCenter, 'extracted/CoilFieldAtCenter.tex', 1);
 
+coilTip = 1/3; %SWAG
+
+verticalField = CoilFieldAtCenter*cos(coilTip);   printSigNumber(verticalField,   'extracted/verticalField.tex'  ,1);
+transverseField = CoilFieldAtCenter*sin(coilTip); printSigNumber(transverseField, 'extracted/transverseField.tex',1);
+axialField = transverseField;			  printSigNumber(axialField,      'extracted/axialField.tex'     ,1);
