b0283505966f3a175f3265d2b2907bf755b7bb0f
charlie <charlie@dicke.nanoradian.com>
2014-03-25 12:55:53 -0700

    ONE RUN REMOVED FROM MERGE, AS IT CONTAINS ERRONEOUS ERRORBARS. MUST CHECK!

--- runAnalysis/run3147pLDAFiltermerge.sh
+++ runAnalysis/run3147pLDAFiltermerge.sh
@@ -19,7 +19,7 @@ cat results/run3166pM3FilterOnly.dat >> results/run3147pM3FilterMerge.dat
 cat results/run3197pM3FilterOnly.dat >> results/run3147pM3FilterMerge.dat
 cat results/run3203pM3FilterOnly.dat >> results/run3147pM3FilterMerge.dat
 cat results/run3204pM3FilterOnly.dat >> results/run3147pM3FilterMerge.dat
-cat results/run3205pM3FilterOnly.dat >> results/run3147pM3FilterMerge.dat
+#cat results/run3205pM3FilterOnly.dat >> results/run3147pM3FilterMerge.dat
 
 ../bin/commentStrip.sh   results/run3147pM3FilterMerge.dat
 ../bin/whiteLineStrip.sh results/run3147pM3FilterMerge.dat
