6a23daef188ef6bfa75fac5b33752d0147bd1c9e
charlie <charlie@dicke.nanoradian.com>
2014-03-26 15:02:40 -0700

    Cleaned up some varFv2 comments

--- bootstrap/varFv2.m
+++ bootstrap/varFv2.m
@@ -7,9 +7,6 @@
 
 function vF = varF(x,sx,B,sB,A,L,C)
 
-%	sx = 0 * sx;
-%	sB = 0 * sB; 
-
 	T = YukFiniteSizeCorrections(L);
 	Q = YukPreFactor;
 
@@ -30,23 +27,14 @@ function vF = varF(x,sx,B,sB,A,L,C)
 	sx2 = sx.^2;
 	ALT2m = ALT * ALT.';
 
-	
-%	InvL = 1./L;
-%	TCross2 = -InvL*transpose(sx2);
-%	for ctr = 1:rows(L)
-%		test2(ctr,:,:) = InvL(ctr)*TCross2;
-%	end
-
+	%Compute arguments to crossterm exponentials
 	for ctri = 1:rows(L)
-	for ctrj = 1:rows(L)
-                 exparg(ctri,ctrj,:) = sx2 * L2ij(ctri,ctrj) / 2.0 - x * Lij(ctri,ctrj);
-                exparg2(ctri,ctrj,:) = -sx2 * LiLj(ctri,ctrj) ;
-                preFactor(ctri,ctrj,:) = ALT2m(ctri,ctrj).* (B(:,ctri) .* B(:,ctrj));
-        end
+		for ctrj = 1:rows(L)
+			 exparg(ctri,ctrj,:) = sx2 * L2ij(ctri,ctrj) / 2.0 - x * Lij(ctri,ctrj);
+			exparg2(ctri,ctrj,:) = -sx2 * LiLj(ctri,ctrj) ;
+			preFactor(ctri,ctrj,:) = ALT2m(ctri,ctrj).* (B(:,ctri) .* B(:,ctrj));
+		end
 	end
-%	ea = exparg;
-%	ea2 = exparg2;
-%	pf = preFactor;
 
 %Reference
 %	for ctr = 1:rows(x)
@@ -55,18 +43,11 @@ function vF = varF(x,sx,B,sB,A,L,C)
 %		preFactor(:,:,ctr) = ALT2m.* (B(ctr,:) * B(ctr,:).');
 %	end
 
-%	1
-%	sum(sum(sum(preFactor)))
-%	2
-%	sum(sum(sum(pf)))
-%	3
-
-%	if(test2 == exparg2)
-%		error('woohoo')
-%	end
-
+	%exponentiate
 	 expval = exp(exparg);
 	expval2 = exp(exparg2);
+	
+	%crossterms
 	crossTerms = expval.*(1 - expval2 ) .* preFactor;
 
 	%Strip the diagonal
@@ -82,35 +63,26 @@ function vF = varF(x,sx,B,sB,A,L,C)
 	IL = 1./L.';
 	IL2= 1./transpose ( L.^2 );
 
-%	 [sx2 * IL2 - 2 * x * IL x sx sx2]
-
-
-
 	%compute vF
 	vF =   (
-		Q^2 * crossTerm	
-		+ Q^2 * exp( 2 * sx2 * IL2 - 2 * x * IL ) .* (sB.^2 + B.^2 .* ( 1 - exp( - sx2 * ( 1 ./ transpose(L.*L) ) ) ) ) * ALT.^2
+		Q^2 *( crossTerm
+		+ exp( 2 * (sx2 * IL2 -  x * IL) ) .* (sB.^2 + B.^2 .* ( 1 - exp( - sx2 * ( 1 ./ transpose(L.*L) ) ) ) ) * ALT.^2 )
 		- 2*C*Q*exp( sx2/2.0 * IL2 - x * IL ) .* (sx2 * IL ) .* B * ALT  ...
 		+ C^2 * sx2
 		);
-%}
-%	vF = 0;
-
-%	save 'plotme2.dat' vF
-%	vF = 0*vF;
 
 	if(isnan(vF))
 		warning('varF2 threw a NaN');
 	end
 
-	svF = std(vF);
-
-	if( vF < -2*svF)
-		min(vF)
-		svF 
-		error('variance of F is less than zero. Furthermore, it is too negative. Fix.');
-	end
-
 end
 
 %Tests - must be positive, if sx and sb are zero, then vF should be zero.
+%	svF = std(vF);
+%
+%	if( vF < -2*svF)
+%		min(vF)
+%		svF 
+%		error('variance of F is less than zero. Furthermore, it is too negative. Fix.');
+%	end
+
