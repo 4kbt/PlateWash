6e59eb3c8501d61253ec8e109b539bca1afbf948
Charlie Hagedorn <cah49@uw.edu>
2014-04-04 12:31:35 -0700

    Plot sizes fixed, pagenumbers visible.

--- bootstrap/generatePlottingTeXSystematics.sh
+++ bootstrap/generatePlottingTeXSystematics.sh
@@ -10,7 +10,7 @@ for names in $(ls output/bootstrapYukawa.Sys*.eps)
 do
 echo '\begin{figure}' >> $fn
 echo '\begin{centering}' >> $fn
-	echo "\\includegraphics[height=0.7\\textheight, angle=270]{$myPathName$names}" >> $fn
+	echo "\\includegraphics[height=0.5\\textheight, angle=270]{$myPathName$names}" >> $fn
 echo '\par\end{centering}' >> $fn
 
 captionVal=`echo $names | sed "s:$myPathName::" | sed "s:output/bootstrapYukawa\.Sys::" | sed 's/\.dat\.eps//'`
