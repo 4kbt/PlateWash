33ace52c61143159096a2d4edaff3587f2411717
Charlie Hagedorn <cah49@uw.edu>
2014-04-14 19:55:14 -0700

    Another day, another tiny bug. Now matches 3164sync3

--- runConfig/WithIFO/run3164sync3i.m
+++ runConfig/WithIFO/run3164sync3i.m
@@ -9,7 +9,7 @@ preSync3
 %psStartSec  = pwStartSec+271;
 %psEndSec    = psEndSec-100;
 %psEndSec    = psEndSec - 86000*.8
-%psEndSec  = psStartSec + 50783
+psEndSec  = psStartSec + 50783
 %pwEndSec    = pwStartSec + 219000; 
 %pwStartSec  = pwStartSec + 6300;
 %ifoEndSec   = ifoStartSec + 16000; 
