3c609ff5ffe2d631a89cc64efbba9171ee330955
Charlie Hagedorn <cah49@uw.edu>
2014-04-15 15:10:21 -0700

    bugfix

--- runAnalysis/pLDAiFilterMerge.sh
+++ runAnalysis/pLDAiFilterMerge.sh
@@ -1,4 +1,4 @@
-cat results/run*iFilterOnly.dat > results/run3147pM3iFilterMerge.dat
+cat results/run*ipM3FilterOnly.dat > results/run3147pM3iFilterMerge.dat
 
 ../bin/commentStrip.sh   results/run3147pM3iFilterMerge.dat
 ../bin/whiteLineStrip.sh results/run3147pM3iFilterMerge.dat
