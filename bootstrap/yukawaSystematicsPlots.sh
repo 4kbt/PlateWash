a808bcc3d1488fd39875a82815075651b25e05ca
Charlie Hagedorn <cah49@uw.edu>
2014-06-11 01:07:11 -0700

    preFitPlotting improved. Bedtime.

--- bootstrap/yukawaSystematicsPlots.sh
+++ bootstrap/yukawaSystematicsPlots.sh
@@ -9,3 +9,11 @@ do
 	gnuplot -e "HOMEDIR = \"$1\"" yukawaSystematicsPlots.gpl
 done
 
+for  PLOTME in `ls output/preFitPlotData.a*.dat`
+do
+	echo $PLOTME
+	export GNUPLOTME=$PLOTME
+
+	gnuplot -e "HOMEDIR = \"$1\"" preFitPlot.gpl 
+done
+
