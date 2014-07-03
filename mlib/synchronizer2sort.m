6bbc63e89284c001247c7d70e3edf522db793e71
Charlie Hagedorn <cah49@uw.edu>
2014-07-03 13:44:16 -0700

    synchronizer2 commented

--- mlib/synchronizer2sort.m
+++ mlib/synchronizer2sort.m
@@ -9,34 +9,35 @@
 
 function out = synchronizer2sort(d1, d2)
 
-	
+	%allocate "out"	
 	out = zeros((rows(d1)+rows(d2)),3);
 
-	%d1 = [d1 ones(rows(d1),1) zeros(rows(d1),4)];
-	%d2 = [d2 zeros(rows(d1),1) ones(rows(d2) zeros(rows(d2),3)];
-	
+	%tag incoming data
 	d1 = [d1 1*ones(rows(d1),1)];
 	d2 = [d2 2*ones(rows(d2),1)];
 	
+	%concatenate
 	D = [d1;d2];
 	
+	%sort on time, get index
 	[ds , is] = sort(D(:,1));
-	
-	DS = D(is,:) ; 
-	
-	now = zeros(1,3);
-	
 
+	%implement the sort, using index
+	DS = D(is,:) ; 
+
+	%pre-loop setup	
+	now = zeros(1,3);
+
+	%loop over sorted data
 	for(ctr = 1:rows(DS))
 	
+		%Update time
 		now(1) = DS(ctr,1);
+
+		%Uses data tag to properly allocate the record.
 		now(DS(ctr,3)+1) = DS(ctr, 2);
 	
 		out(ctr,:) = now;
-	
-	        %if(mod(ctr,10000) == 0)
-	        	%ctr/rows(DS)
-	       % end
 	end
 	
 	
