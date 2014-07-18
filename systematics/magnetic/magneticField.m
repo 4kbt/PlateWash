d88b4db7587d157a0a5cbc77c55e41cb90eb382c
Charlie Hagedorn <cah49@uw.edu>
2014-07-18 14:37:50 -0700

    typo

--- systematics/magnetic/magneticField.m
+++ systematics/magnetic/magneticField.m
@@ -6,6 +6,6 @@ printSigNumber(CoilFieldAtCenter, 'extracted/CoilFieldAtCenter.tex', 1);
 
 coilTip = 1/3; %SWAG
 
-verticalField = CoilFieldAtCenter*cos(coilTip);   printSI(verticalField, , 1, -6, 'T', 'extracted/verticalField.tex'  );
+verticalField = CoilFieldAtCenter*cos(coilTip);   printSI(verticalField,   1, -6, 'T', 'extracted/verticalField.tex'  );
 transverseField = CoilFieldAtCenter*sin(coilTip); printSI(transverseField, 1, -6, 'T', 'extracted/transverseField.tex');
 axialField = transverseField;			  printSI(axialField,      1, -6, 'T', 'extracted/axialField.tex'     );
