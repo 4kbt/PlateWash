3b9c996dff700adfc8ab018e44896a47dd5a751b
Charlie Hagedorn <cah49@uw.edu>
2014-07-17 17:52:09 -0700

    minor addendum to last bugfix.

--- systematics/pressure/finalPressure.m
+++ systematics/pressure/finalPressure.m
@@ -4,4 +4,4 @@ d = load('finalPressure.dat');
 
 printSI(d, 1, -9, 'torr', 'extracted/finalPressureTorr.tex');
 
-printSI(d*OnePascalInTorr, 1, -6,'Pa', 'extracted/finalPressurePa.tex', 2);
+printSI(d*OnePascalInTorr, 1, -6,'Pa', 'extracted/finalPressurePa.tex');
