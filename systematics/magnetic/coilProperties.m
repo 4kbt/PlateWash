811b981cc1fa87ee0ea878a2481e4351e947d3bc
Charlie Hagedorn <cah49@uw.edu>
2014-07-20 09:46:20 -0700

    Typo.

--- systematics/magnetic/coilProperties.m
+++ systematics/magnetic/coilProperties.m
@@ -1,11 +1,11 @@
 %Checked 1/7/2014
 CoilDiameter = 31.5*0.0254;
 CoilDiameterErr = 0.25*0.0254;
-printSIErr(CoilDiameter, CoilDiameterErr, 2, -3, 'extracted/CoilDiameter.tex')
+printSIErr(CoilDiameter, CoilDiameterErr, 2, -3, 'm', 'extracted/CoilDiameter.tex')
 
 CoilTurns = 190*2;
 printInteger( CoilTurns, 'extracted/CoilTurns.tex');
 
 CoilCurrent = 0.8;
 CoilCurrentErr = 0.1;
-printSI(CoilCurrent, CoilCurrentErr, 1, -3, 'A', 'extracted/CoilCurrent.tex');
+printSIErr(CoilCurrent, CoilCurrentErr, 1, -3, 'A', 'extracted/CoilCurrent.tex');
