0b8425952ec8d655c02212bc7f63a58c9ccdc9bb
Charlie Hagedorn <cah49@uw.edu>
2014-07-25 17:50:36 -0700

    Dubious commit - switching to jpgs. Breaks postscript output?

--- bootstrap/generatePlottingTeXSystematics.sh
+++ bootstrap/generatePlottingTeXSystematics.sh
@@ -9,12 +9,13 @@ echo '' > $fn
 for names in $(ls output/bootstrapYukawa.Sys*.dat.eps)
 do
 #Begin including triangle plot
-pname=`echo $names | sed 's/\.dat\.eps/\.dat.plt\.eps/'`
+#pname=`echo $names | sed 's/\.dat\.eps/\.dat.plt\.eps/'`
+pname=`echo $names | sed 's/\.dat\.eps/\.dat.plt\.jpg/'`
 
 echo '\begin{figure}' >> $fn
 echo '\begin{centering}' >> $fn
-	#echo "\\includegraphics[height=0.5\\textheight, angle=270,natwidth=3240,natheight=3240]{$myPathName$pname}" >> $fn
-	echo "\\includegraphics[height=0.5\\textheight, angle=270]{$myPathName$pname}" >> $fn
+	echo "\\includegraphics[height=0.5\\textheight, angle=270,natwidth=3240,natheight=3240]{$myPathName$pname}" >> $fn
+	#echo "\\includegraphics[height=0.5\\textheight, angle=270]{$myPathName$pname}" >> $fn
 echo '\par\end{centering}' >> $fn
 
 captionVal=`echo $names | sed "s:$myPathName::" | sed "s:output/bootstrapYukawa\.Sys::" | sed 's/\.dat\.eps//'`
