4a1bdb30741ecdda788a45bf8479d849e05b5fe3
Charlie Hagedorn <cah49@uw.edu>
2014-07-30 17:48:11 -0700

    Working through units cleanup and general housekeeping.

--- globalConfig/run3147PendulumParameters.m
+++ globalConfig/run3147PendulumParameters.m
@@ -16,20 +16,35 @@ HOMEDIR = getenv("HOMEDIR");
 %%%%% PENDULUM TiTa %%%%%%
 
 %        'jan13Pendulum'
-
 rhoTi=4507;
 printSI( rhoTi, 2, 3 ,'g/m$^3$',  [HOMEDIR '/extracted/titaniumDensity.tex' ]);
-pendulumBodyHeight=0.0321203; printSigNumber(pendulumBodyHeight, [HOMEDIR '/extracted/pendulumBodyHeight.tex' ],4);
-pendulumBodyWidth =0.0431448; printSigNumber(pendulumBodyWidth,  [HOMEDIR '/extracted/pendulumBodyWidth.tex'  ],4); %How did I get a part in 10^6? Pg 57 of 
-pendulumBodyThickness= 0.070*0.0254; printSigNumber(pendulumBodyThickness , [HOMEDIR '/extracted/pendulumBodyThicknessNoSI.tex'],2);
-printSIErr(pendulumBodyThickness, 0.001*0.0254, 1, -3, 'm', [HOMEDIR '/extracted/pendulumBodyThickness.tex' ]); %1 part in 10^2 seems coarse?
+pendulumBodyHeight=0.0321203;
+pendulumBodyHeightErr = 0.005*0.0254; %Safe SWAG, see pg 57
+printSIErr(pendulumBodyHeight, pendulumBodyHeightErr, 1, -3, 'm',[HOMEDIR '/extracted/pendulumBodyHeight.tex' ]);
+pendulumBodyWidth = 0.0431448; 
+pendulumBodyWidthErr = 0.005*0.0254; %Safe SWAG, see pg 57
+printSIErr(pendulumBodyWidth,  pendulumBodyWidthErr,  1, -3, 'm',[HOMEDIR '/extracted/pendulumBodyWidth.tex'  ]); 
+pendulumBodyThickness    = 0.070*0.0254; %See pg 50 for pre-machined thicknesses
+pendulumBodyThicknessErr = 0.004*0.0254; %unclear how much material removed by lapping.
+printSIErr(pendulumBodyThickness , pendulumBodyThicknessErr, 1, -3, 'm', [HOMEDIR '/extracted/pendulumBodyThickness.tex']);
+%The following output is used elsewhere?
+printSigNumber(pendulumBodyThickness , [HOMEDIR '/extracted/pendulumBodyThicknessNoSI.tex'],2);
 pendulumBodyMass=rhoTi*pendulumBodyHeight*pendulumBodyWidth*pendulumBodyThickness;
+%Before etch/lapping, body with inlay cuts was 9.6143g (pg 54)
+%After all filing, total pendulum mass was 13.8602g. (pg 55)
 
 rhoTaP=16650; printSI(rhoTaP, 2, 3, 'g/m$^3$', [HOMEDIR '/extracted/inlayDensity.tex' ]);
 rhoInlay=rhoTaP-rhoTi;
 inlayHeight=pendulumBodyHeight;
-inlayThickness=0.010*0.0254 ; printSigNumber(inlayThickness, [HOMEDIR '/extracted/inlayThickness.tex' ], 2);
-inlayWidth=pendulumBodyWidth-27.610e-3 ; printSigNumber(inlayWidth, [HOMEDIR '/extracted/inlayWidth.tex' ], 5);
+
+inlayThickness=0.008*0.0254 ; %see pg 51
+inlayThicknessErr = 0.002*0.0254; %see pg 51 
+printSIErr(inlayThickness, inlayThicknessErr, 1, -6, 'm', [HOMEDIR '/extracted/inlayThickness.tex' ]);
+
+inlayWidth = pendulumBodyWidth-1.087*0.0254 ; %See pg 50
+inlayWidthErr = 0.005*0.0254; %conservative.
+printSIErr(inlayWidth, inlayWidthErr, 1, -3, 'm', [HOMEDIR '/extracted/inlayWidth.tex' ]);
+
 inlayMass=rhoInlay*inlayHeight*inlayThickness*inlayWidth; printSigNumber(inlayMass, [HOMEDIR '/extracted/inlayMass.tex' ], 2);
 
 
