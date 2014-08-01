5f9ba68c53148fbd043d81ab34e3b3fc8c5d0b57
Charlie Hagedorn <cah49@uw.edu>
2014-07-31 17:59:47 -0700

    facing pages for fits/triangles?

--- bootstrap/generatePlottingTeXSystematics.sh
+++ bootstrap/generatePlottingTeXSystematics.sh
@@ -12,7 +12,8 @@ do
 #pname=`echo $names | sed 's/\.dat\.eps/\.dat.plt\.eps/'`
 pname=`echo $names | sed 's/\.dat\.eps/\.dat.plt\.jpg/'`
 
-echo '\begin{figure}' >> $fn
+echo '\begin{figure}[p]' >> $fn
+echo '\begin{leftfullpage}' >> $fn
 echo '\begin{centering}' >> $fn
 	echo "\\includegraphics[height=0.85\\textheight, angle=270,natwidth=3240,natheight=3240]{$myPathName$pname}" >> $fn
 	#echo "\\includegraphics[height=0.5\\textheight, angle=270]{$myPathName$pname}" >> $fn
@@ -21,12 +22,13 @@ echo '\par\end{centering}' >> $fn
 captionVal=`echo $names | sed "s:$myPathName::" | sed "s:output/bootstrapYukawa\.Sys::" | sed 's/\.dat\.eps//'`
 
 echo "\\caption{Triangle plot for $captionVal\. A signal was injected at \$\\alpha=a\$, \$\\lambda=l\$, and with a linear slope of {\\tt slop}. Plus, Null, and Minus are with the foil correction added, not-added, and subtracted. }" >> $fn
-
+echo  '\end{leftfullpage}' >> $fn
 echo  '\end{figure}' >> $fn
 echo  '\clearpage' >> $fn
 
 #Begin analysis plot
 echo '\begin{figure}' >> $fn
+echo '\begin{fullpage}' >> $fn
 echo '\begin{centering}' >> $fn
 	echo "\\includegraphics[height=0.5\\textheight, angle=270]{$myPathName$names}" >> $fn
 echo '\par\end{centering}' >> $fn
@@ -35,6 +37,7 @@ captionVal=`echo $names | sed "s:$myPathName::" | sed "s:output/bootstrapYukawa\
 
 echo "\\caption{Analysis results for $captionVal}" >> $fn
 
+echo  '\end{fullpage}' >> $fn
 echo  '\end{figure}' >> $fn
 echo  '\clearpage' >> $fn
 
