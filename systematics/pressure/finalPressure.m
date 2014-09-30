c2ecc676a567c59b97f2023a9641ba3ab43890c9
charlie <charlie@dicke.nanoradian.com>
2014-09-29 21:47:44 -0700

    Squeeze-film gas damping

--- systematics/pressure/finalPressure.m
+++ systematics/pressure/finalPressure.m
@@ -1,7 +1,20 @@
 fundamentalConstants
+run3147FixedParameters
 
 d = load('finalPressure.dat');
 
 printSI(d, 1, -9, 'torr', 'extracted/finalPressureTorr.tex');
 
-printSI(d*OnePascalInTorr, 1, -6,'Pa', 'extracted/finalPressurePa.tex');
+pd = d*OnePascalInTorr
+
+printSI(pd, 1, -6,'Pa', 'extracted/finalPressurePa.tex');
+
+
+%taken from Levy Paper -- 1/QP = 5 at P = ~90 um.
+
+QGas = 1/(5*pd);
+
+%According to Stephan's wallet card for external damping, 
+NoisePowerOfOmega = 4 * k_B * 293 * kappa /QGas / (2*pi*pendulumF0);
+
+printSI(sqrt(NoisePowerOfOmega), 1, -15, 'N-m/$\sqrt{\mbox{Hz}}$', 'extracted/squeezeFilmNoisePower.tex');
