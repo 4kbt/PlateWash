11d4d781af30bab617ec8a5a8c706f937da7d161
Charlie Hagedorn <cah49@uw.edu>
2014-11-07 12:39:48 -0800

    You reap what you sow. Temporary comments removed.

--- bootstrap/yukawaSystematicsPlots.sh
+++ bootstrap/yukawaSystematicsPlots.sh
@@ -12,13 +12,13 @@ do
 	gnuplot -e "HOMEDIR = \"$1\"; gravityOnly = 1" yukawaSystematicsPlots.gpl
 done
 
-#for  PLOTME in `ls output/*.plt`
-#do
-#	echo $PLOTME
-#	export GNUPLOTME=$PLOTME
-#
-#	gnuplot -e "HOMEDIR = \"$1\"" preFitPlot.gpl
-#	#convert ${PLOTME}.eps -resize 3240x3240 -quality 96 ${PLOTME}.png  
-#	convert ${PLOTME}.eps -flatten -resize 3240x3240 ${PLOTME}.jpg  
-#done
+for  PLOTME in `ls output/*.plt`
+do
+	echo $PLOTME
+	export GNUPLOTME=$PLOTME
+
+	gnuplot -e "HOMEDIR = \"$1\"" preFitPlot.gpl
+	#convert ${PLOTME}.eps -resize 3240x3240 -quality 96 ${PLOTME}.png  
+	convert ${PLOTME}.eps -flatten -resize 3240x3240 ${PLOTME}.jpg  
+done
 
