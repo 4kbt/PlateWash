3f8daae81313bd230fab7903560e1858f5c92ca6
charlie <charlie@watson>
2014-11-14 12:27:54 -0800

    first pass at consolidating date-parsing.

---
+++ mlib/dateTimeToSecs.m
@@ -0,0 +1,63 @@
+%converts dates/times as column vectors to the number of seconds elapsed since Jan 1, 2009, 0:00:00.
+function secs = dateTimeToSecs( mdyy, hms)
+
+	if(mdyy(3)!=9 & mdyy(3)!=10 & mdyy(3)!= 11 & mdyy(3) != 12)
+		error("Not 2009 or 2010 or 2011 or 2012!");
+	end
+
+	switch mdyy(1)
+		case {1}
+			secs=0;
+		case {2}
+			secs=86400*(31);
+		case {3}
+			secs=86400*(31+28);
+		case {4}
+			secs=86400*(31+28+31);
+		case {5}
+			secs=86400*(31+28+31+30);
+		case {6}
+			secs=86400*(31+28+31+30+31);
+		case {7}
+			secs=86400*(31+28+31+30+31+30);
+		case {8}
+			secs=86400*(31+28+31+30+31+30+31);
+		case {9}
+			secs=86400*(31+28+31+30+31+30+31+31);
+		case {10}
+			secs=86400*(31+28+31+30+31+30+31+31+30);
+		case {11}
+			secs=86400*(31+28+31+30+31+30+31+31+30+31);
+		case {12}
+			secs=86400*(31+28+31+30+31+30+31+31+30+31+30);
+		otherwise
+			error("Not a month!");
+	endswitch
+
+	if(mdyy(3) == 10)
+		secs = secs + 86400*365;
+	end
+
+	if(mdyy(3) == 11)
+		secs = secs + 86400*365*2;
+	end
+
+	%Added 1-3-2012 CAH
+	if(mdyy(3) == 12)
+		secs = secs + 86400*365*3;
+		%Leap Year
+		if ( mdyy(1) > 2 )
+			 secs = secs + 86400;
+		end
+	end
+
+	timeConversion=[3600 60 1];
+	secs += (86400*mdyy(2) + timeConversion * hms);
+end
+
+%!test
+%! mdyy = [ 2 14 12 ]';
+%! hms = [ 1 43 31 ]';
+%! outSecs = dateTimeToSecs(mdyy, hms)
+%! testSecs = sum([ 365*3*86400 + 86400*(31 + 14) + 3600*1 + 60*43 + 31] )
+%! assert( outSecs == testSecs); 
--- mlib/psLoadHdr.m
+++ mlib/psLoadHdr.m
@@ -16,60 +16,7 @@ psStartDate = fscanf(psHeaderFile, '%g/%g/%g\n');
 fscanf(psHeaderFile,'%c',14);
 psStartTime= fscanf(psHeaderFile, '%g:%g:%g\n');
 
-if(psStartDate(3)!=9 & psStartDate(3)!=10 & psStartDate(3) != 11 & psStartDate(3) != 12)
-        error("Not 2009 or 2010 or 2011 or 2012!");
-end
-
-
-switch psStartDate(1)
-	case {1}
-		psStartSec=0;
-	case {2}
-		psStartSec=86400*(31);
-	case {3}
-		psStartSec=86400*(31+28);
-	case {4}
-		psStartSec=86400*(31+28+31);
-	case {5}
-		psStartSec=86400*(31+28+31+30);
-	case {6}
-		psStartSec=86400*(31+28+31+30+31);
-	case {7}
-		psStartSec=86400*(31+28+31+30+31+30);
-	case {8}
-		psStartSec=86400*(31+28+31+30+31+30+31);
-	case {9}
-		psStartSec=86400*(31+28+31+30+31+30+31+31);
-	case {10}
-		psStartSec=86400*(31+28+31+30+31+30+31+31+30);
-	case {11}
-		psStartSec=86400*(31+28+31+30+31+30+31+31+30+31);
-	case {12}
-		psStartSec=86400*(31+28+31+30+31+30+31+31+30+31+30);
-	otherwise
-		error("Not a month!");
-endswitch
-
-if(psStartDate(3) == 10)
-        psStartSec = psStartSec + 86400*365;
-end
-
-if(psStartDate(3) == 11)
-        psStartSec = psStartSec + 86400*365*2;
-end
-
-%Added 1-3-2012 CAH
-if(psStartDate(3) == 12)
-        psStartSec = psStartSec + 86400*365*3;
-        %Leap Year
-        if ( psStartDate(1) > 2 )
-                 psStartSec = psStartSec + 86400;
-        end
-end
-
-
-timeConversion=[3600 60 1];
-psStartSec += (86400*psStartDate(2) + timeConversion * psStartTime);
+psStartSec = dateTimeToSecs(psStartDate, psStartTime); 
 
 % # columns of data 
 fscanf(psHeaderFile,'%c',18);
