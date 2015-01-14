0862cb096ff93c98e4b4a58b183f6a70d1ef3570
Charlie Hagedorn <cah49@uw.edu>
2015-01-13 16:09:45 -0800

    all the way through the first proofreading pass.

--- bootstrap/arbFitPositions.m
+++ bootstrap/arbFitPositions.m
@@ -3,7 +3,7 @@ arbFitx2Spacing = 50e-6;        printSI(arbFitx2Spacing, 2, -6, 'm', 'extracted/
 arbFitx1Start   = shortCut; printSI(arbFitx1Start  , 2, -6, 'm', 'extracted/arbFitx1Start.tex'  , 2);
 arbFitx1Stop    = 300e-6;       printSI(arbFitx1Stop   , 3, -6, 'm', 'extracted/arbFitx1Stop.tex'   , 2);
 arbFitx2Start   = arbFitx1Stop; printSI(arbFitx2Start  , 3, -6, 'm', 'extracted/arbFitx2Start.tex'  , 2);
-arbFitx2Stop    = 900e-6;       printSI(arbFitx2Start  , 3, -6, 'm', 'extracted/arbFitx2Stop.tex'   , 2);
+arbFitx2Stop    = 900e-6;       printSI(arbFitx2Stop   , 3, -6, 'm', 'extracted/arbFitx2Stop.tex'   , 2);
 
 xpos1 = arbFitx1Start:arbFitx1Spacing:arbFitx1Stop;
 xpos2 = arbFitx2Start:arbFitx2Spacing:arbFitx2Stop;
