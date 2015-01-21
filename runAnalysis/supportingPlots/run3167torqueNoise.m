eebf9c30e9e559309d4a4f43c8c1ac1569921076
Charlie Hagedorn <cah49@uw.edu>
2015-01-21 13:54:58 -0800

    Chp 4 plots upgraded, details polished.

--- runAnalysis/supportingPlots/run3167torqueNoise.m
+++ runAnalysis/supportingPlots/run3167torqueNoise.m
@@ -15,4 +15,22 @@ RSLN = [pwData(lnStart:lnEnd, pwTimeCol), pwData(lnStart:lnEnd, torqueCol)];
 save 'run3167timeData.dat' RS;
 save 'run3167timeDataLN.dat' RSLN
 save 'run3167fft.dat' PF;
-save 'run3167fftLN.dat' LN 
+save 'run3167fftLN.dat' LN
+
+freqs = 10.^( (-3.1:.01:0) ) ;
+freqs = freqs';
+
+ResponseFunction = 1/kappa ./ ...
+	sqrt( ( 1 - freqs.^2 / pendulumF0^2 ).^2 + 1/pendulumQ^2 );
+
+AutocollimatorLimit = ...
+	autocolNoise ./ ResponseFunction; 
+
+ThermalLimit =  sqrt( 4 * k_B * 293 * kappa / pendulumQ ./ ( 2 * pi * freqs ) );
+
+AL = [ freqs AutocollimatorLimit ]; 
+TL = [ freqs ThermalLimit ];
+
+save 'run3167ThermalLimit.dat' TL;
+save 'run3167AutocollimatorLimit.dat' AL;
+
