2d30523c339bb41b902de45602632e0b6aba5bbc
Charlie Hagedorn <cah49@uw.edu>
2015-03-02 23:53:34 -0800

    Hand-entered quantities now feed into table.

--- globalConfig/run3147MetrologyParameters.m
+++ globalConfig/run3147MetrologyParameters.m
@@ -6,3 +6,8 @@ bounceTouchPosition   = -892e-6;
 bounceAngleOffset     = 0.00058;
 bounceAngleOffsetUncertainty = 200e-6; 
 
+%all three are +-, so double for absolute distance
+%should consider an area correction for pendulum
+PendulumFlatness  = 7.5e-6 ;
+AttractorFlatness = 4.5e-6 ;
+FoilFlatness      = 2e-6   ;
---
+++ metrology/eyeballQuantities.m
@@ -0,0 +1,5 @@
+run3147MetrologyParameters
+
+printSI( PendulumFlatness  , 2, -6, 'm', 'extracted/pendulumFlatness.tex' );
+printSI( AttractorFlatness , 2, -6, 'm', 'extracted/attractorFlatness.tex');
+printSI( FoilFlatness      , 1, -6, 'm', 'extracted/foilFlatness.tex'     );
