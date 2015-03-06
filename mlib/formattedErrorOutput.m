8e65d1402485c6c459a382918354dc6f9010c3fb
Charlie Hagedorn <cah49@uw.edu>
2015-03-06 15:49:54 -0800

    Bugfix, had called wrong printDecimal function.

--- mlib/formattedErrorOutput.m
+++ mlib/formattedErrorOutput.m
@@ -3,9 +3,9 @@
 %prefix is the string prepended to each filename
 function formattedErrorOutput( values , precision, prefix )
 
-	printDecimal( values(1) , precision , [ prefix 'Central.tex'       ] );
-	printDecimal( values(2) , precision , [ prefix 'Statistical.tex'   ] );
-	printDecimal( values(3) , precision , [ prefix 'Systematic.tex'    ] );
-	printDecimal( values(4) , precision , [ prefix 'AggregateError.tex'] );
+	printFixedPrecisionDecimal( values(1) , precision , [ prefix 'Central.tex'       ] );
+	printFixedPrecisionDecimal( values(2) , precision , [ prefix 'Statistical.tex'   ] );
+	printFixedPrecisionDecimal( values(3) , precision , [ prefix 'Systematic.tex'    ] );
+	printFixedPrecisionDecimal( values(4) , precision , [ prefix 'AggregateError.tex'] );
 
 endfunction
