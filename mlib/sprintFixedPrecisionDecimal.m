cde225b85b42f82a26875bb3e4ef3c9dcee72c57
Charlie Hagedorn <cah49@uw.edu>
2015-03-08 10:33:08 -0700

    crippling sprintFixedPrecisionDecimal in hopes of placating tabular output.

--- mlib/sprintFixedPrecisionDecimal.m
+++ mlib/sprintFixedPrecisionDecimal.m
@@ -1,6 +1,10 @@
 %prints a TeXed fixed-precision decimal number to a string
 %number of decimalPlaces counts after the decimal. 1 --> 0.1 , 2 --> 0.12 , etc.
 %this function could be extended to fixed rounding modes with ease
+%
+%This is presently configured to do the right thing spacing-wise for tables
+%Appending a '%%' to the end of the formatted string will do the right thing
+%for text
 
 function s = sprintFixedPrecisionDecimal( number , numberOfDecimalPlaces )
 
@@ -12,11 +16,11 @@ function s = sprintFixedPrecisionDecimal( number , numberOfDecimalPlaces )
 	number = round( number * scaleFactor ) / scaleFactor;
 
 	%print string
-	s = sprintf( '$%.*f$%%' , numberOfDecimalPlaces , number);
+	s = sprintf( '$%.*f$' , numberOfDecimalPlaces , number);
 
 endfunction
 
-%!test assert( sprintFixedPrecisionDecimal( 1.234   , 2) == '$1.23$%'  )
-%!test assert( sprintFixedPrecisionDecimal( 250.315 , 2) == '$250.32$%')
-%!test assert( sprintFixedPrecisionDecimal( 250.    , 2) == '$250.00$%')
-%!test assert( sprintFixedPrecisionDecimal( 1.2345  , 3) == '$1.235$%' )
+%!test assert( sprintFixedPrecisionDecimal( 1.234   , 2) == '$1.23$'  )
+%!test assert( sprintFixedPrecisionDecimal( 250.315 , 2) == '$250.32$')
+%!test assert( sprintFixedPrecisionDecimal( 250.    , 2) == '$250.00$')
+%!test assert( sprintFixedPrecisionDecimal( 1.2345  , 3) == '$1.235$' )
