e5078b8f95f2a9b1c83ff008ad6d9a3fc9b9c1bf
Charlie Hagedorn <cah49@uw.edu>
2015-03-12 11:41:31 -0700

    Well, temperature change of 0.5 didn't speed things up, what about this?

--- bootstrap/bootstrapYukawaSystematics.m
+++ bootstrap/bootstrapYukawaSystematics.m
@@ -80,7 +80,7 @@ for bootStrapCounter = 1:(NumberOfYukawaBootstraps)  % three covers add, null, s
 		tic
 
 		%Do the fit.
-		[x , csMin, convergence, details] = samin("chiSquareWSystematics", {trimmedPM, ranSeed', PendStruct, AttrStruct, CNStruct}, {LowerBounds', UpperBounds', 20, 5, 0.5, 10*iterAve, 5, 1e-3, 0.1, 1, 2});
+		[x , csMin, convergence, details] = samin("chiSquareWSystematics", {trimmedPM, ranSeed', PendStruct, AttrStruct, CNStruct}, {LowerBounds', UpperBounds', 20, 5, 0.1, 10*iterAve, 5, 1e-1, 0.1, 1, 2});
 
 		%0 = no convergence, 1 = good, 2 = near edge
 		fitInfo = convergence; 
