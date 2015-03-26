c115c43d0c528683d0b887df2bf40adc41127fe9
Charlie Hagedorn <cah49@uw.edu>
2015-03-25 17:01:26 -0700

    prettified trimPM

--- bootstrap/trimPM.m
+++ bootstrap/trimPM.m
@@ -6,15 +6,15 @@ function T = trimPM(pM,  signalColumns, fitColumn)
         columnNames;
         fitErrColumn = diffErrOffset + fitColumn;
 
-	T.x1Vec = pM(:,aCol);
-        T.x2Vec = pM(:,bCol);
+	T.x1Vec  = pM(:,aCol);
+        T.x2Vec  = pM(:,bCol);
         T.sx1Vec = pM(:,aErrCol);
         T.sx2Vec = pM(:,bErrCol);
 
         %Dynamic allocation of signal and error columns.
         [T.BMat T.sBMat] = allocateSignalColumns(signalColumns, ABErrOffset, pM);
 
-	T.fitCol = pM(:,fitColumn);
+	T.fitCol    = pM(:,fitColumn);
 	T.fitErrCol = pM(:,fitErrColumn);
 
 end
