5840404e3586d9b657a2da835a19b7b0ce24949a
charlie <charlie@dicke.nanoradian.com>
2015-06-04 17:47:13 -0700

    It's a thesis!

--- globalConfig/run3147FixedParameters.m
+++ globalConfig/run3147FixedParameters.m
@@ -133,7 +133,7 @@ torqueBlur   = 1e-12;  printSI(torqueBlur, 1, -15, 'N-m', [HOMEDIR 'extracted/to
 %fprintf('# read Complete \n')
 
 %'INSUFFICENT bootstrap counts'
-NumberOfYukawaBootstraps = 563; %was 1000
+NumberOfYukawaBootstraps = 480; %was 1000
 NumberOfArbFitBootstraps = NumberOfYukawaBootstraps; % was 300
 printInteger( NumberOfYukawaBootstraps , [HOMEDIR '/extracted/numBootstraps.tex']);
 
